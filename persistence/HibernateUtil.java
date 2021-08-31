f8703b7c7827391c306ec2cd514e781d8ddff9bb
Fran <franmastucci@gmail.com>
2021-08-31 13:20:42 -0300

    nada

--- persistence/HibernateUtil.java
+++ persistence/HibernateUtil.java
@@ -9,7 +9,7 @@ import org.hibernate.cfg.Configuration;
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory = buildSessionFactory();
-
+    							
     private static SessionFactory buildSessionFactory() {
 
     	final String propertiesURL = "hibernate.properties";
